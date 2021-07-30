54bd06f46797d1b8a8aadff86e6ba9a2aab9a977
mathav95raj <mathav95raj@gmail.com>
2021-07-30 18:45:27 +0530

    12

--- app.py
+++ app.py
@@ -84,7 +84,7 @@ def predict_stage(pth):
     activations = activation["fc1"]
     test_xgb_f = torch.cat((test_xgb_f, activations.detach().cpu()), dim=0)
     test_xgb_f = test_xgb_f.detach().numpy()
-    # test_xgb_f = Fit.transform(test_xgb_f)
+    test_xgb_f = Fit.transform(test_xgb_f)
     y = y.max(1)[1].item()
     st.write("CNN prediction: ", itol[y])
     del y
